a2666d39fc9eecefb5475dee6c0b3e991b6bdc6b
Shakibuzzaman Sakib <kenDua.com@gmail.com>
2025-08-25 18:20:25 +0000

    আমাদের সম্পর্কে নামে একটি পেইজ করে দেন। এখানে এ বিষয়ে তথ্য থাকবে। আমাদের

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -114,7 +114,7 @@ export default async function ContactPage() {
               </div>
               <div className="h-80 md:h-96 w-full rounded-xl overflow-hidden shadow-lg">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.879940198188!2d90.39053831543154!3d23.75167699462529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8bd552c2b3b%3A0x4e70f1178de1f6f3!2sDhaka!5e0!3m2!1sen!2sbd!4v1678886363028!5m2!1sen!2sbd"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.879940198188!2d90.39053831543154!3d23.75167699462529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8bd552c2b3b%3A0x4e70f1f1178de1f6f3!2sDhaka!5e0!3m2!1sen!2sbd!4v1678886363028!5m2!1sen!2sbd"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
